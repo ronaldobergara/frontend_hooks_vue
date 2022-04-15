a2891e314fe281647a41e28da50ed411c8484ac2
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-15 12:03:01 -0300

    Configuraçao do projeto

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+}
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false
+})
