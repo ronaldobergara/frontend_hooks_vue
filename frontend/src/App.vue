e323161b70f12a1e87eb42aa84d068b332754c66
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-15 12:03:29 -0300

    Hooks BeforeCreate e Created

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ message }}!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      message: 'Hello Kaori'
+    }
+  },
+  beforeCreate() {
+    console.log('Antes da Criação')
+    console.log(this.message)
+  },
+  created() {
+    console.log('Depois da Criação')
+    console.log(this.message)
+  }
+})
+</script>
+
+<style></style>
